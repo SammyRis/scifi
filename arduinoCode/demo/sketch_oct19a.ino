d4b7221222394f191915dc63f23156a7fe7f5dd4
Sam Risling <60522761+SammyRis@users.noreply.github.com>
2022-10-22 08:33:47 -0600

    Added motor code
    Motor control adapted for use with PNP transisor

---
+++ arduinoCode/demo/sketch_oct19a.ino
@@ -0,0 +1,23 @@
+int countDir = 1;
+int motorSpeed = 2;
+int motorPin = 11;
+int testPin = A5;
+void setup(){
+  Serial.begin(9600);
+  pinMode(motorPin, OUTPUT);
+}
+
+void loop(){
+  motorSpeed += countDir;
+  
+  if(motorSpeed > 90){
+    countDir = -1;
+  }
+  else if(motorSpeed < 1){
+    countDir = 1;
+  }
+  
+  analogWrite(motorPin, motorSpeed);
+  Serial.println(analogRead(testPin));
+  delay(50);
+}
